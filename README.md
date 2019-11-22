6853cf6cb254f31bb034927333a4ae98e4329d27
Glitch (exer-track-fcc) <none>
2019-11-22 18:17:18 +0000

    🐔🌒 Updated with Glitch - Updated Nov 22, 2019 at 12:17 pm

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# exer_track_fcc
+freeCodeCamp API and Microservices Project - Exercise Tracker
+=================
+
+See https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker for the objectives of this project.
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+
+mongoose.connect(process.env.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('useCreateIndex', true);
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.use('/api/exercise', require('./controllers/addHandler'));
+app.use('/api/exercise', require('./controllers/getMembers'));
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT || 5000, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
